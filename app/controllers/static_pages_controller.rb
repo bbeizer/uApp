f8b6f38d6f66354a3bab199f02f8d1cb39372ce9
bbeizer <89622436+bbeizer@users.noreply.github.com>
2022-09-20 00:23:18 -0400

    changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,7 @@ class StaticPagesController < ApplicationController
   def about
   end
 
+  def help
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contact'
+  get '/help',    to: 'static_pages#help', as: :help
+  get '/about',    to: 'static_pages#help', as: :about
+ 
   resources :users
   get 'signup' => 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
 end

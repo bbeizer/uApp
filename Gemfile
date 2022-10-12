5f7fc3f1fe628c982c1bed176c960559ab78e043
bbeizer <89622436+bbeizer@users.noreply.github.com>
2022-10-11 21:23:13 -0400

    commits

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ group :development do
   gem 'listen', '~> 3.3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'will_paginate',           '3.3.0'
+  gem 'bootstrap-will_paginate', '1.0.0'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get 'sessions/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
   get '/help',    to: 'static_pages#help', as: :help
   get '/about',    to: 'static_pages#help', as: :about
- 
-  resources :users
   get 'signup' => 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  resources :account_activations, only: [:edit]
+  resources :users
 
 end
